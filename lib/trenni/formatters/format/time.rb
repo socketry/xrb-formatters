8bd771ca4d3db8afc15eb39996e3ad0e90032562
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2016-03-10 02:36:23 +1300

    Useful helpers?

---
+++ lib/trenni/formatters/format/time.rb
@@ -0,0 +1,39 @@
+# Copyright, 2012, by Samuel G. D. Williams. <http://www.codeotaku.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+require 'trenni/strings'
+require 'mapping/model'
+
+module Trenni::Formatters::Format
+	module Time
+		def included(base)
+			base.map(Time) do |object, options|
+				# Ensure we display the time in localtime, and show the year if it is different:
+				if object.localtime.year != Time.now.year
+					object.localtime.strftime("%B %-d, %-l:%M%P, %Y")
+				else
+					object.localtime.strftime("%B %-d, %-l:%M%P")
+				end
+			end
+			end
+		end
+	end
+end
+
